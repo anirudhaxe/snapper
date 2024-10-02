f4a5b9d56993410de2db85af61c30f6fd5149c96
Aniruddha Chaudhary <aniruddha@Aniruddhas-MacBook-Air.local>
2024-10-02 19:16:51 +0530

    chore: updated sst-env.d.ts

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -17,6 +17,10 @@ declare module "sst" {
       "type": "sst.aws.Queue"
       "url": string
     }
+    "deleteBackupQueue": {
+      "type": "sst.aws.Queue"
+      "url": string
+    }
     "snapperDbBackupBucket": {
       "name": string
       "type": "sst.aws.Bucket"
